4d9b6e50069fe8557a86ee909d9abea7d2902dde
Nico <ncfortuin27@gmail.com>
2020-06-17 21:10:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+ 
+  count: number = 0;
+  text: string = "Sorry but you can't go passed 0";
+
+  handleIncrease = () => {
+
+    if(this.count === 10){
+      this.count = 0;
+    }
+
+    this.count = this.count + 1;
+  }
+
+  handleDecrease = () => {
+ 
+    if(this.count > 0){
+      this.count = this.count - 1;
+    } else{
+      this.count = 0
+    }
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
+
+
 }
